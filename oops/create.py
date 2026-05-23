f58db30b63474f9d5fb7b6d198b03aba287f1e1f
Jit Ghosh <jitghosh304@gmail.com>
2026-05-23 23:47:50 +0530

    create2

--- oops/create.py
+++ oops/create.py
@@ -1,9 +1,18 @@
-class student:
-    #blueprint
-    def __init__(self,name,rollno, attendance):
+class Student:
+    # blueprint
+    def __init__(self, name, rollno, attendance):
         self.name = name
         self.rollno = rollno
         self.attendance = attendance
-        
-        s1=student("sachin", 1, 90):
-        print(s1.name)
+
+
+s1 = Student("sachin", 1, 90)
+print(s1.name)
+
+s2 = Student("sourav", 2, 85)
+print(s2.name)
+
+s3 = Student("virat", 3, 95)
+print(s3.name)
+
+print(s1.attendance)
