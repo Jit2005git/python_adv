d5de0e1d5c6d9b32b323494dcdf91fab8f6726d9
Jit Ghosh <jitghosh304@gmail.com>
2026-05-23 23:41:11 +0530

    create1

---
+++ oops/create.py
@@ -0,0 +1,9 @@
+class student:
+    #blueprint
+    def __init__(self,name,rollno, attendance):
+        self.name = name
+        self.rollno = rollno
+        self.attendance = attendance
+        
+        s1=student("sachin", 1, 90):
+        print(s1.name)
