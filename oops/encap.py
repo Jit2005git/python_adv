869a465b308e3e59922061d0c141c2bfdcea444d
Jit Ghosh <jitghosh304@gmail.com>
2026-05-26 21:30:01 +0530

    balance

--- oops/encap.py
+++ oops/encap.py
@@ -1,4 +1,9 @@
 class BankAccount:
     def init__(self,name,balance):
         self.name =name
-        
+        self.__balance = balance # private variable
+
+
+acc1 = BankAccount("John", 1000)
+#acc1.__balance = 2000 # direct access to private variable
+print(acc1.__balance)
