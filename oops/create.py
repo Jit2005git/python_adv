a614e5e794c0c7bceb1df0327b808e60a79486e2
Jit Ghosh <jitghosh304@gmail.com>
2026-05-26 17:47:13 +0530

    grade_cal

--- oops/create.py
+++ oops/create.py
@@ -1,24 +1,33 @@
 class Student:
     # blueprint
-    def __init__(self, name, rollno, attendance):
+    def __init__(self, name, marks, attendance):
         self.name = name
-        self.rollno = rollno
+        self.marks = marks
         self.attendance = attendance
 
+    def calculate_grade(self):
+            if self.marks >= 90:
+                return "A"
+            elif self.marks<=80:
+                return "B"
+            else:
+                return "C"
 
-s1 = Student("sachin", 1, 90)
+s1 = Student("sachin", 90, 85)
 print(s1.name)
-
-s2 = Student("sourav", 2, 85)
+print(s1.calculate_grade())
+s2 = Student("sourav", 85, 90)
 print(s2.name)
 
-s3 = Student("virat", 3, 95)
+s3 = Student("virat", 95, 80)
 print(s3.name)
-print(s1.attendance)
-print(s2.attendance)
-print(s3.attendance)
 
-print(s1.attendance)
-print(s2.attendance)
-print(s3.attendance)
+
+# print(s1.attendance)
+# print(s2.attendance)
+# print(s3.attendance)
+
+# print(s1.attendance)
+# print(s2.attendance)
+# print(s3.attendance)
 
