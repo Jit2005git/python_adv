bf27cdeb3edd5f9c0c13f840011111e1952d8968
Jit2005git <jitghosh304@gmail.com>
2026-07-18 00:21:00 +0530

    abs3

--- oops/abstr.py
+++ oops/abstr.py
@@ -1,14 +1,19 @@
-class Animal():
-    print("Animal makes sound")
+# class Animal():
+#     print("Animal makes sound")
 
-class Dog(Animal):
-    def sound (self):
-        print("Dog barks")
-class Cat(Animal):
-    def sound (self):
-        print("Cat meows")
+# class Dog(Animal):
+#     def sound (self):
+#         print("Dog barks")
+# class Cat(Animal):
+#     def sound (self):
+#         print("Cat meows")
 
-animals = [Dog(), Cat()]
-for a in animals:
-    a.sound()   
-    
+# animals = [Dog(), Cat()]
+# for a in animals:
+#     a.sound()   
+from abc  import ABC, abstractmethod
+class Animal(ABC):
+    @abstractmethod
+    def sound(self):
+        pass
+a= Animal()   
