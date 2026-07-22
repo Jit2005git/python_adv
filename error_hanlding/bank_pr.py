a782528ba4f138bfe1f7dad942928fa665a1adc8
Jit2005git <jitghosh304@gmail.com>
2026-07-22 14:33:36 +0530

    bank

---
+++ error_hanlding/bank_pr.py
@@ -0,0 +1,15 @@
+class InsufficientBalanceError(Exception):
+    pass
+class BankAccount:
+    def __init__(self, balance):
+        self.balance = balance
+    def withdraw(self, amount):
+        if amount > self.balance:
+            raise InsufficientBalanceError("Insufficient balance in the account")
+        self.balance -= amount
+        print(f"Withdrawal successful. New balance: {self.balance}")
+try:
+    acc=BankAccount(1000)
+    acc.withdraw(1500)
+except InsufficientBalanceError as e:
+    print("Transaction failed:", e)
