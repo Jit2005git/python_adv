1660c0ac6d40632e277bb01654112d2fbbb98b77
Jit2005git <jitghosh304@gmail.com>
2026-07-23 23:38:23 +0530

    append

--- fileHanlding/fileop.py
+++ fileHanlding/fileop.py
@@ -1,4 +1,8 @@
 # file = open("fileHanlding\\input.txt",  "r")#r read mode
-file = open("fileHanlding\\input2.txt",  "w")#w write mode
-file.write("Hello, World!")
+# file.read()
+# file = open("fileHanlding\\input2.txt",  "w")#w write mode overwrite
+# file.write("Hello, World!")
+file=open("fileHanlding\\input2.txt",  "a")#a append mode
+file.write("\nappended text")
+
 file.close()
